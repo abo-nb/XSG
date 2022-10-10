463724790e4bc4d8dd032c23317e35fe1793b35e
abo-nb <84396950+abo-nb@users.noreply.github.com>
2022-10-10 18:10:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var v =true;
+
 document.onkeydown=function(e){    //对整个页面监听  
     var keyNum=window.event ? e.keyCode :e.which;       //获取被按下的键值  
     //判断如果用户按下了回车键（keycody=13）  
@@ -7,3 +9,31 @@ document.onkeydown=function(e){    //对整个页面监听
         window.location.replace(url)
     }  
 }
+
+function set(){
+   
+    if(v){
+        document.getElementById('v').src='img/visibility_off_FILL0_wght400_GRAD0_opsz48.png';
+        v=false
+        document.getElementById("qb").style.visibility="hidden";
+    }else
+    {
+        document.getElementById('v').src='img/visibility_FILL0_wght400_GRAD0_opsz48.png';
+        v=true
+        document.getElementById("qb").style.visibility="visible";
+
+    }
+}
+
+function v(){
+    alert("123");
+
+}
+
+var nowTime ;
+function play(){
+var time = new Date();
+nowTime = time.getHours()+":"+time.getMinutes();
+document.getElementById("time").innerHTML = nowTime;
+}
+setInterval(play,1000);//每一秒执行一次
